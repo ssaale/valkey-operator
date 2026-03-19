64d88ea96bc154ff48013f2f6a54851cf19d5497
Lee, Bong Won <Lee@bong.won>
2026-03-19 15:22:47 +0900

    remove unnecessary files

--- cmd/init_sentinel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf /data/announce.conf
-
-echo "check pod binded service"
-/opt/valkey-helper sentinel expose || exit 1
-
-# copy binaries
-cp /opt/*.sh /opt/valkey-helper /mnt/opt/ && chmod 555 /mnt/opt/*.sh /mnt/opt/valkey-helper
